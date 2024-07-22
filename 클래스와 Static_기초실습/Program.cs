9d594471a24638137e0c1e9e280172b3a7279c1b
leesiyeon99 <qsqs9090@naver.com>
2024-07-22 16:14:10 +0900

    [Add] 클래스와 Static_기초실습

---
+++ 클래스와 Static_기초실습/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace 클래스와_Static_기초실습
+{
+    internal class Program
+    {
+        static class MyCalculator
+        {
+            public static double Add(double first, double second)
+            {
+                return first + second;
+            }
+
+            public static double Subtract(double first, double second)
+            {
+                return first - second;
+            }
+
+            public static double Multiply(double first, double second)
+            {
+                return first * second;
+            }
+
+            public static double Divide(double first, double second)
+            {
+                if (second == 0)
+                {
+                    Console.WriteLine("두번째 값은 0이 될 수 없습니다.");
+                    return 0;
+                }
+                else
+                return first / second;
+
+            }
+
+            public static double Squarded(double first, double second)
+            {
+                double sum = 1;
+                for (int i = 0; i < second; i++)
+                {
+                    sum *= first;
+                }
+                return sum;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyCalculator.Squarded(2,3));
+            Console.WriteLine(MyCalculator.Divide(4,0));
+        }
+    }
+}
